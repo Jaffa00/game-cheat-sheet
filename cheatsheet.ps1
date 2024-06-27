5d20d47541aa71fe0a80666bd4f67fbdbdb67062
Jeff Standen <jeff@standen.dev>
2024-06-27 06:58:15 +0100

    working on json and tab setup

--- cheatsheet.ps1
+++ cheatsheet.ps1
@@ -13,8 +13,8 @@ param(
 
 if (!(Test-Path $ConfigJSON)) {
     #JSON not found
-   Write-Host "JSON file not found"
-   exit 1
+    Write-Host "JSON file not found"
+    exit 1
 }
 $ConfigJSON = "./examples/ds3/ds3.json"
 $JSONfile = get-item $ConfigJSON
@@ -26,8 +26,8 @@ write-host "folder is $($JSONfile.Directory)"
 if ($PSBoundParameters.ContainsKey("OutputFolder")) {
     if (!Test-Path $OutputFolder) {
         #JSON not found
-       Write-Host "Output folder not found"
-       exit 2
+        Write-Host "Output folder not found"
+        exit 2
     }
 }
 else {
@@ -48,6 +48,69 @@ $JSONObject.singles | ForEach-Object {
 
     $tHTML = $tHTML.Replace("{{$($_.find)}}", $_.replace)
 }
+
+#tabs
+
+$boolFirst = true
+$navlinks=""
+$tabdata=""
+
+$JSONObject.tabs | ForEach-Object {
+    if ($boolFirst) {
+        $navlinks+= '<li class="active">'
+        $tabdata += "      <!-- $($_.title.ToUpper()) START -->'n      <div class=""tab-pane active"" id=""tab$tab"">`n"
+        $boolFirst = false
+    }
+    else {
+        $navlinks += '<li>'
+        $tabdata += "      <!-- $($_.title.ToUpper()) START -->'n      <div class=""tab-pane"" id=""tab$tab"">`n"
+    }
+    $tab = [System.Web.HttpUtility]::UrlEncode($_.title)
+    $navlinks= += "<a href=""#tab$tab"" data-toggle=""tab"" data-target=""#tab$tab,#btnHideCompleted"">$($_.title)</a></li>`n"
+
+    #tabdata - the real meat is here
+    #filters
+    
+    
+    $tabdata += '<h2>Filter Checklist '
+    #NG  don't really like this
+    if ($_.ngtoggle) {
+        $tabdata += '      <span class="btn-group btn-group-toggle" data-toggle="buttons">
+            <label class=""btn btn-default""><input type="radio" name="journey" data-ng-toggle="1">NG</label>
+            <label class="btn btn-default"><input type="radio" name="journey" data-ng-toggle="2">NG+</label>
+            <label class="btn btn-default"><input type="radio" name="journey" data-ng-toggle="3">NG++</label>
+          </span>`n'     
+    }
+    $tabdata += '    </h2>`n'
+
+    #filter buttons
+    $tabdata +='<div class="btn-group">`n'
+
+    foreach ($filter in $_.filters) {
+
+     $tabdata +=    '<div class="btn-group btn-group-vertical">
+            <div class="btn-group">
+              <div class="btn-group-vertical" data-toggle="buttons">
+                <label class="btn btn-default dropdown-toggle">
+                  <input type="checkbox" data-category-toggle />
+                  <span class="glyphicon glyphicon-eye-close"></span>
+                  <span class="glyphicon glyphicon-eye-open"></span>
+                  ' + $filter.category + '
+                </label>
+              </div>
+            </div>'
+    }
+    #foreach item here, then end div
+    
+    
+
+
+}
+
+
+$tHTML = $tHTML.Replace("{{NAVLINKS}}", $navlinks)
+
+$tHTML = $tHTML.Replace("{{TABDATA}", $tabdata)
 
 
 
